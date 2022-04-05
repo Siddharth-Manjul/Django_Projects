50d43393541e7f47bf5d90edc4b5927c455efb3b
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-04-06 00:30:25 +0530

    2nd commit

--- message_board/posts/models.py
+++ message_board/posts/models.py
@@ -1,3 +1,11 @@
+from cgitb import text
+from xml.parsers.expat import model
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+          text = models.TextField()
+          
+          def __str__(self):
+                    return self.text[:50]
+          
